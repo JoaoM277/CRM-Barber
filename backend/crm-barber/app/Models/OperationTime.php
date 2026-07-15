919ed0a153f74bd1cef8036c3cfbfb031206a598
FranklynSantos <franklynsantos451@gmail.com>
2026-07-15 12:14:21 -0300

    Cria as Models e Relacionametos

---
+++ backend/crm-barber/app/Models/OperationTime.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OperationTime extends Model
+{
+    /** @use HasFactory<\Database\Factories\OperationTimeFactory> */
+    use HasFactory;
+
+    protected $table = 'operation_time';
+
+    protected $fillable = [
+        'day_of_week',
+        'start_time',
+        'waiting_start',
+        'waiting_end',
+        'end_time'
+    ];
+
+}
