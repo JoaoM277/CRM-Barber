9de607e2deaa8c1b92eb6d4310dd50af6159c822
FranklynSantos <franklynsantos451@gmail.com>
2026-07-15 19:22:00 -0300

    Cria o ClientController
    Primeira versao do Client Controller

---
+++ backend/crm-barber/app/Http/Controllers/ClientController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $clients = Client::all();
+
+        return response()->json($clients);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $client = Client::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'birth_date' => $request->birth_date,
+            'observation' => $request->observation
+        ]);
+
+        return response()->json($client, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Client $client)
+    {
+        return response()->json($client);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Client $client)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Client $client)
+    {
+        $client->update($request->all());
+
+        return response()->json(['message:' => 'Updated Successfully'], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Client $client)
+    {
+        $client->delete();
+
+        return response()->json([
+            'message' => 'Client removed successfully!'
+        ]);
+        
+    }
+}
